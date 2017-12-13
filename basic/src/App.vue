332e571938bcabdd0ef84d4f0abc3cf0006de2cb
Moey Design <5884seiya@gmail.com>
2017-12-13 17:44:07 -0500

    first commit

---
+++ basic/src/App.vue
@@ -0,0 +1,28 @@
+<template>
+  <div id="app">
+    <Home />
+  </div>
+</template>
+
+<script>
+import Home from './components/Home'
+require('reset-css')
+require('vue2-animate/dist/vue2-animate.min.css')
+
+export default {
+  name: 'app',
+  components: {
+    Home
+  }
+}
+</script>
+
+<style>
+html, body, #app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  height: 100%;
+  background-color: snow;
+}
+</style>
---
+++ basic/src/components/Home.vue
@@ -0,0 +1,39 @@
+<!-- =================================================
+ Vue Template
+================================================== -->
+<template>
+  <div class="page-wrapper">
+    <div class="box-wrapper">
+      <div class="box">
+
+      </div>
+    </div>
+  </div>
+</template>
+
+<!-- =================================================
+ Vue Script
+================================================== -->
+<script>
+// import anime from 'animejs'
+
+export default {
+  name: 'home'
+}
+</script>
+
+<!-- =================================================
+ Vue Style
+================================================== -->
+<style scoped>
+.page-wrapper {
+  padding: 20px;
+}
+
+.box {
+  width: 400px;
+  height: 300px;
+  background-color: seagreen;
+  border-radius: 20px;
+}
+</style>
